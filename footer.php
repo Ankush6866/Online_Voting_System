0ee0a81cee10044182a66e9caef5c6691b67d307
Ankush6866 <150901539+Ankush6866@users.noreply.github.com>
2024-05-05 20:37:04 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+    <div class="row bg-black text-center text-white">
+        <div class="col-12 my-3">
+            <p> &copy; Copyright 2022 - All Rights Reserved  <br />
+                Developed by AnkushChauhan.com
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
